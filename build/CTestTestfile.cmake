61e00d8cae19fda5cfe86ff3d2bbcad0a36a1958
Daniel Corrêa <eldestemido@gmail.com>
2021-12-20 03:33:06 -0300

    Remove some folders. i.e (.vscode and build)

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/C++/SFML-BOOK/startGame
-# Build directory: /home/daniel/C++/SFML-BOOK/startGame/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
