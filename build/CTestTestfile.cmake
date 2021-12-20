af5de390563f14a1aa5e20100e45d338e9e93ff3
Daniel Corrêa <eldestemido@gmail.com>
2021-12-20 03:26:00 -0300

    Started a SFML with CMake Project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/C++/SFML-BOOK/startGame
+# Build directory: /home/daniel/C++/SFML-BOOK/startGame/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
